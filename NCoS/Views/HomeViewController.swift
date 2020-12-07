da8887b630953d31a90ec06a2c20c8428c4fd29e
pomalone91 <pomalone91@gmail.com>
2020-12-06 17:00:22 -0700

    HomeView is now just one downView

--- NCoS/Views/HomeViewController.swift
+++ NCoS/Views/HomeViewController.swift
@@ -13,9 +13,7 @@ import WebKit
 /**
  This will be the primary view for the tab contr@objc @objc oller. This will show the five most recently published articles. I think I can add a stack view and then programatically add downViews for each article
  */
-class HomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, WKNavigationDelegate {
-    
-    @IBOutlet var tableView: UITableView!
+class HomeViewController: UIViewController, WKNavigationDelegate {
     
     var articles = [Article]()
     var downViews = [DownView]()
@@ -35,69 +33,19 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
         let blog = Blog(string: "http://ninecirclesofshell.com/api/get-service.php")
         articles = Array(blog.articles.sorted(by: <).prefix(5))
         
-        // Load downViews
-        for article in articles {
-            if let dv = try? DownView(frame: self.view.bounds, markdownString: article.contents, openLinksInBrowser: true, templateBundle: bundle, writableBundle: true, configuration: nil, options: .default, didLoadSuccessfully: nil) {
+        let markdown = homeMarkDownString(from: articles)
+        
+        if let dv = try? DownView(frame: self.view.bounds, markdownString: markdown, openLinksInBrowser: true, templateBundle: bundle, writableBundle: true, configuration: nil, options: .default, didLoadSuccessfully: nil) {
 //                print(article.title)
 //                dv.scrollView.isScrollEnabled = false
 //                dv.scrollView.bounces = false
-                dv.navigationDelegate = self
+//            dv.navigationDelegate = self
 //                dv.webView(dv, didFinish: .none)
-                downViews.append(dv)
-            }
+//            downViews.append(dv)
+            self.view.addSubview(dv)
         }
-        
     }
-    
-    // MARK: - Section set up
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return articles.count
-    }
-    
-    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return articles[section].title
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
-    }
-    
-    // MARK: - Cell set up
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        let downView = downViews[indexPath.row]
-        downView.reload()
-//        print("bounds.height: \(downView.bounds.height)")
-//        print("contentSize.height: \(downView.scrollView.contentSize.height)")
-//        return downView.bounds.height
-//        return downViews[indexPath.row].scrollView.contentSize.height
-//        webView(downView, didFinish: .some(.init()))
-//        print(contentHeight)
-        return self.contentHeight
-        
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        // Make cell
-        let cell = tableView.dequeueReusableCell(withIdentifier: "homeCell", for: indexPath) as! HomeViewCell
-        
-        
-        // Make DownView
-//        let article = articles[indexPath.row]
-//        guard let downView = try? DownView(frame: cell.contentView.bounds, markdownString: article.contents, openLinksInBrowser: true, templateBundle: bundle, writableBundle: true, configuration: nil, options: .default, didLoadSuccessfully: nil) else {
-//            return cell
-//        }
-        
-//        print(indexPath.section)
-        // Add downView to cell
-        cell.update(with: downViews[indexPath.section])
-//        cell.update(with: articles[indexPath.section], bundle: bundle)
-        
-        return cell
-    }
-    
-    
-    
+
     /*
     // MARK: - Navigation
 
@@ -111,27 +59,23 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
     
 }
 
-// MARK: - Extension to set webView height
+// MARK: - Private Helpers
 extension HomeViewController {
-    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-//        print("Content Height \(webView.scrollView.contentSize.height)");
-        let height =  webView.scrollView.contentSize.height
-        self.contentHeight = height - 720
-        webView.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: height)
-        self.tableView.reloadData()
+    
+    /**
+     Builds a contiguous Markdown string of all 5 homescreen articles and displays them in one DownView.
+     1. Add h1 to top of each string with line break
+     2. Download all images for each article
+     3. Add --- horizontal breaks between articles
+     */
+    private func homeMarkDownString(from articles: [Article]) -> String {
+        var homeString = ""
+        
+        for article in articles {
+            homeString.append(contentsOf: "# \(article.title)\n")   // Add h1 header from title
+            homeString.append(contentsOf: article.contents)
+            homeString.append(contentsOf: "\n\n----\n")                   // Add horizontal rule after article
+        }
+        return homeString
     }
 }
-
-//
-//extension DownView {
-//    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-//        self.webView.evaluateJavaScript("document.readyState", completionHandler: { (complete, error) in
-//            if complete != nil {
-//                self.webView.evaluateJavaScript("document.body.scrollHeight", completionHandler: { (height, error) in
-//                    self.containerHeight.constant = height as! CGFloat
-//                })
-//            }
-//
-//            })
-//    }
-//}
